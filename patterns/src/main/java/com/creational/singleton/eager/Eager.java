70832d024bdb4cb3ba13d9369c5d5cb665669a47
StasOmelchenko <omelchenko.stas.1997@gmail.com>
2018-02-08 23:43:04 +0200

    Implemented Singleton design pattern using eager initialization

---
+++ patterns/src/main/java/com/creational/singleton/eager/Eager.java
@@ -0,0 +1,14 @@
+package com.creational.singleton.eager;
+
+/**
+ * The main class of implementation Singleton design pattern using eager initialization
+ * 
+ * @author Stas Omelchenko
+ */
+
+public class Eager {
+
+	public static void main(String[] args) {
+
+	}
+}
